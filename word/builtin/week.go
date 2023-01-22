98e81b53c98e4815214512bae0e9019528e2ce2c
ning.wang <ning.wang@chaitin.com>
2023-01-22 18:50:13 +0800

    chore: 优化内置单词组

--- word/builtin/week.go
+++ word/builtin/week.go
@@ -3,13 +3,13 @@ package builtin
 var Week = Group{
 	Name: "week(builtin)",
 	Words: []Word{
-		{English: "Monday", Chinese: "周一"},
-		{English: "Tuesday", Chinese: "周二"},
-		{English: "Wednesday", Chinese: "周三"},
-		{English: "Thursday", Chinese: "周四"},
-		{English: "Friday", Chinese: "周五"},
-		{English: "Saturday", Chinese: "周六"},
-		{English: "Sunday", Chinese: "周日"},
+		{English: "Monday", Chinese: "星期一", Query: "星期一"},
+		{English: "Tuesday", Chinese: "星期二", Query: "星期二"},
+		{English: "Wednesday", Chinese: "星期三", Query: "星期三"},
+		{English: "Thursday", Chinese: "星期四", Query: "星期四"},
+		{English: "Friday", Chinese: "星期五", Query: "星期五"},
+		{English: "Saturday", Chinese: "星期六", Query: "星期六"},
+		{English: "Sunday", Chinese: "星期日", Query: "星期日"},
 		{English: "weekend", Chinese: "周末"},
 	},
 }
