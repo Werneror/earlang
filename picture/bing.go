39200ba0f2bbf01be5c3ab6760797705341b8741
Werner <me@werner.wiki>
2023-05-04 21:49:53 +0800

    feat: 获取图片时支持翻页

--- picture/bing.go
+++ picture/bing.go
@@ -22,28 +22,35 @@ func (b *BingImageSearch) Name() string {
 
 func (b *BingImageSearch) WordPictures(word string, number int) ([]string, error) {
 	pageNum := 0
-	count := number + 10 // 有时候搜索结果少于指定的搜索数量，多搜索几个
-	baseURL := "https://cn.bing.com/images/async?q=%s&first=%d&count=%d&relp=%d&tsc=ImageBasicHover&datsrc=I&layout=RowBased&mmasync=1"
-	body, err := common.ReqGET(fmt.Sprintf(baseURL, url.QueryEscape(word), pageNum*count, count, count))
-	if err != nil {
-		return nil, fmt.Errorf("query bing images search for %s error: %w", word, err)
-	}
-	defer body.Close()
-	doc, err := goquery.NewDocumentFromReader(body)
-	if err != nil {
-		return nil, fmt.Errorf("parse bing images search for %s error: %w", word, err)
-	}
 	picUrls := make([]string, 0, number)
-	doc.Find("img").EachWithBreak(func(i int, s *goquery.Selection) bool {
-		href, exists := s.Attr("src")
-		if exists && strings.HasPrefix(href, "http") {
-			href = strings.Split(href, "?")[0] // url 中可能用 ?w=223&h=180 指定尺寸，这里去掉
-			picUrls = append(picUrls, href)
-			if len(picUrls) >= number {
-				return false
-			}
+	for {
+		baseURL := "https://cn.bing.com/images/async?q=%s&first=%d&count=%d&relp=%d&tsc=ImageBasicHover&datsrc=I&layout=RowBased&mmasync=1"
+		body, err := common.ReqGET(fmt.Sprintf(baseURL, url.QueryEscape(word), pageNum*number, number, number))
+		if err != nil {
+			return nil, fmt.Errorf("query bing images search for %s error: %w", word, err)
 		}
-		return true
-	})
+		defer body.Close()
+		doc, err := goquery.NewDocumentFromReader(body)
+		if err != nil {
+			return nil, fmt.Errorf("parse bing images search for %s error: %w", word, err)
+		}
+		doc.Find("img").EachWithBreak(func(i int, s *goquery.Selection) bool {
+			href, exists := s.Attr("src")
+			if exists && strings.HasPrefix(href, "http") {
+				href = strings.Split(href, "?")[0] // url 中可能用 ?w=223&h=180 指定尺寸，这里去掉
+				picUrls = append(picUrls, href)
+				if len(picUrls) >= number {
+					return false
+				}
+			}
+			return true
+		})
+		// 有时候一页搜索结果里凑不够指定数量的图片，需要翻页
+		pageNum += 1
+		// 为防止死循环，限制最大页数为 5
+		if pageNum > 5 || len(picUrls) >= number {
+			break
+		}
+	}
 	return picUrls, nil
 }
